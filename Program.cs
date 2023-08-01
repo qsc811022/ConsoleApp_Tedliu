c785ce3d96b5947f990bcf5bf3b343ffca8b8a85
劉敦桐 <zse811022@hotmail.com>
2023-08-02 06:59:09 +0800

    20230802

--- Program.cs
+++ Program.cs
@@ -4,7 +4,60 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!"); // First_Learn
+            /*Console.WriteLine("Hello, World!");*/ // First_Learn
+            //int height;
+            //height = 180; // 設定變數值為 180
+            //int sum = 5 + 12; // sum 等於 17
+            //int difference = 10 - 2; // 10 減 2 等於 8
+            //int result = 3 * 5; // result 等於 3 * 5 =15, * 表示相乘
+            //int quotient = 12 / 6; // quotient 等於 12 除以 6 ,等於 2, 斜線表示相除, 兩數相除, 也就是取得商數的意思
+
+            //int result1 = 1 + 2 * 3 - 6;
+            //Console.WriteLine(result1);
+
+            //Q1
+            //先心算以下計算式, 再寫程式驗證結果與您想的是否相同
+            //1 + (2 + 2) * 3 - 1
+            //2 + ((2 + 1) / 3 + 1) * 2
+
+            //Q2
+            //寫程式, 計算並顯示 120 除以 7 的商數, 餘數, 各是多少
+            //int Q2=120/7;
+            //int Q22=120%7;
+            //Console.WriteLine(Q2);
+            //Console.WriteLine(Q22);
+
+            ////Q3 寫程式, 計算並顯示 1 + 2 + 3 + .... + 10 的總和
+            //int sum=0;
+            //for (int i = 0; i < 11; i++)
+            //{
+            //    sum+=i;
+            //}
+            //Console.WriteLine(sum);
+            //int number1 = 10, number2 = 99;
+
+            //// step 1,先用 temp變數存放 number1 值
+            //int temp = number1;
+
+            //// step 2, 異動 number1 值
+            //number1 = number2;
+
+            //// step 3, 異動 number2 值
+            //number2 = temp;
+
+            //Console.WriteLine($"對調的結果: number1={number1}, number2={number2}");
+
+
+            for (int i = 1; i <=9; i++)
+            {
+                for(int j = 1; j <=9; j++)
+                {
+                    Console.WriteLine(i+"*"+j+"="+i*j);
+                }
+                Console.WriteLine();
+            }
+
+
         }
     }
 }
